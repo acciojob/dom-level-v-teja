04a7090178d49a6917169893d8fbb3d779dc1575
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-04-28 16:59:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var element = document.getElementById("level");
+var domLevel = 0;
+
+while (element) {
+  domLevel++;
+  element = element.parentNode;
+}
+
+alert("The level of the element is: " + domLevel);
